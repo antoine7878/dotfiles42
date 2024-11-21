7cd2425bff5d282d23df1e960ce1edbada2684d0
ale-tell <antoine.letellier90@gmail.com>
2024-11-21 15:57:20 +0100

    auto

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ ln -s ~/dotfiles/nvim	~/.config/
 ln -s ~/dotfiles/kitty	~/.config/
 ln -s ~/dotfiles/tmux	~/.config/
 ln -s ~/dotfiles/.zshrc	~/
+ln -s ~/dotfiles/.gitconfig .gitconfig
+ln -s ~/dotfiles/.p10k.zsh .p10.zsh
+
 
 python3 -m pip install --upgrade pip setuptools
 
