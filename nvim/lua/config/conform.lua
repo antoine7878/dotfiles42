f38f8f0ac5a120ca7b7a34fbc403f51c0df5b8be
Antoine Le Tellier <antoine.letellier90@gmail.com>
2025-04-28 09:40:36 +0200

    a

--- nvim/lua/config/conform.lua
+++
@@ -1,9 +0,0 @@
-require('conform').setup({
-	formatters_by_ft = {
-      lua = { "stylua" },
-      fish = { "fish_indent" },
-      sh = { "shfmt" },
-      py = { "flake8" },
-      python = { "flake8" },
-    },
-})
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,22 +17,31 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
-  },
-
+  }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
@@ -41,3 +50,40 @@ require("lazy").setup({
     },
   },
 })
+
+require("telescope").setup({
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules/.*",
+      ".git/.*",
+      "*.o",
+    },
+  },
+  pickers = {
+    find_files = {
+      find_command = {
+        "find",
+        ".",
+        "-type",
+        "f",
+        "-name",
+        "*.cpp",
+        "-o",
+        "-name",
+        "*.hpp",
+        "-o",
+        "-name",
+        "Makefile",
+        "-o",
+        "-name",
+        ".lua",
+      },
+      -- find_command = { "rg", "--files", "--color", "never", "--no-require-git" }
+    },
+  },
+})
+
+require("onedark").setup({
+  style = "deep",
+})
+require("onedark").load()
